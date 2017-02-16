155054ce7c956526e51f0f57a74e2ae1bc57463b
Daniel Rice <Daniel.L.Rice@gmail.com>
2017-02-16 10:49:31 +0000

    Plotting as an RGB matrix.

--- shabam.py
+++ shabam.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import pysam
-
+import numpy as np
+import matplotlib.pyplot as plt
+    
 ENDCHAR = "-"
 GAPCHAR = "."
 DELCHAR = "*"
@@ -15,22 +17,40 @@ BAM_CPAD = 6 # P
 BAM_CEQUAL = 7 # =
 BAM_CDIFF = 8 # X
 
+# BASE2COLORS = {
+#     'A': 'green', 
+#     'C': 'blue', 
+#     'G': 'orange', 
+#     'T': 'red',
+#     '-' : 'black',       # deletion
+#     'I' : 'mediumpurple' # insertion
+# }
 
-def ParseCigar(cigar, nucs):
+BASE2COLORS = {
+    'A': np.array([0,110,0], dtype=np.uint8), 
+    'C': np.array([0,0,255], dtype=np.uint8),
+    'G': np.array([255,150,50], dtype=np.uint8),
+    'T': np.array([255,0,0], dtype=np.uint8),
+    '-' : 'pink',       # deletion
+    'I' : 'mediumpurple' # insertion
+}
+
+def ParseCigar(cigar, bases):
     """
     Return list of strings, each item corresponding to a single reference position
     """
     rep = []
     currentpos = 0
     wasinsert = False
+    # print(cigar)
     for operation, length in cigar:
 #         print(operation, length)
         if operation in [BAM_CMATCH, BAM_CEQUAL, BAM_CDIFF]: # match (M, X, =)
             for i in range(length):
                 if wasinsert:
-                    rep[-1] = rep[-1] + nucs[currentpos]
+                    rep[-1] = rep[-1] + bases[currentpos]
                 else:
-                    rep.append(nucs[currentpos])
+                    rep.append(bases[currentpos])
                 wasinsert = False
                 currentpos += 1
         elif operation == BAM_CINS: # put insertion in next base position (I)
@@ -58,26 +78,55 @@ def ParseCigar(cigar, nucs):
     return rep
 
 
+def GenerateRGB(representations, start, end):
+    RGB = np.ones((len(representations), end-start, 3), dtype=np.uint8)*255
+    for ri, representation in enumerate(representations):
+        bases = representation['bases']
+        position = representation['position']
+
+        if position < start:
+            range_start = start - position
+        else:
+            range_start = 0
+
+    #     print('position',position)
+    #     print('len(bases)',len(bases))
+    #     print('end',end)
+        if (position + len(bases)) > end:
+            range_end = (end - position)
+        else:
+            range_end = len(bases)
+            
+        assert range_start < range_end
+        for ci in range(range_start, range_end):
+            base = bases[ci]
+            RGB[ri, ci, :] = BASE2COLORS[base]
+
+    return RGB
+
 def main():
+
     samfile = pysam.AlignmentFile("test/example.bam", "rb")
 
     chrom=16
     start=48000000
     end=48000080
 
+
     reads = samfile.fetch("16", start, end)
-    representations = {}
+    representations = []
     for read in reads:
         position = read.pos
         bases = read.query
         cigar = read.cigartuples
     #     if read.is_reverse:
     #         bases = bases.lower()
-        representations[position] = ParseCigar(cigar, bases)
-    print(representations)
+        representations.append({'position': position, 'bases': ParseCigar(cigar, bases)})
 
-
-
+    RGB = GenerateRGB(representations, start, end)
+    fig, (ax1) = plt.subplots(figsize=(20,10))
+    ax1.imshow(RGB)
+    fig.savefig('testing.png')
 
 if __name__ == '__main__':
     main()
