de2f378f98823de2b67dcafee2c9eb6afcbd7e07
Jeremy McRae <jm33@sanger.ac.uk>
2017-04-11 14:02:55 +0100

    include vertical gridlines every ten bases

--- shabam.py
+++ shabam.py
@@ -193,6 +193,30 @@ def plot_axis(context, start, end, axis_offset):
         
         pos += increment
 
+def plot_grid(context, start, end, axis_offset, height):
+    ''' plot vertical lines every ten bases, so we can orient ourselves
+    
+    Args:
+        context: cairocffi.Context as a plotting device
+        start: nucleotide position at start of plotting window
+        end: nucleotide position at end of plotting window
+        axis_offset: height to account for in axis plotting.
+        height: total height of the plotted image in pixels.
+    '''
+    
+    context.set_source_rgba(0.5, 0.5, 0.5, 0.5)
+    context.set_line_width(1)
+    
+    pos = start + -start % 10
+    while pos <= end:
+        x_pos = (pos - start) * 10
+        
+        context.move_to(x_pos, axis_offset)
+        context.line_to(x_pos, height)
+        context.stroke()
+        
+        pos += 10
+
 def generateRGB(context, reads, start, end, axis_offset, height, ref_seq=None, by_strand=False):
     ''' plots reads to the Context
     
@@ -225,6 +249,7 @@ def generateRGB(context, reads, start, end, axis_offset, height, ref_seq=None, b
             by_strand)
     
     plot_axis(context, start, end, axis_offset - 10)
+    plot_grid(context, start, end, axis_offset, height)
 
 def to_alpha(qual, threshold=35):
     ''' convert base quality to an alpha transparency float
