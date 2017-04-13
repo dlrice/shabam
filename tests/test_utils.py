8dc60e32baef745024d15ddfc8cb8e0781d160c5
Jeremy McRae <jm33@sanger.ac.uk>
2017-04-13 21:28:33 +0100

    added unit tests for determining output format, and image height

---
+++ tests/test_utils.py
@@ -0,0 +1,60 @@
+
+import os
+import unittest
+
+import cairocffi as cairo
+
+from shabam.utils import get_height, fileformat
+
+class TestUtils(unittest.TestCase):
+    
+    def test_get_height(self):
+        ''' check that we can get the image height in pixels
+        '''
+        
+        path = os.path.join(os.path.dirname(__file__), 'example.bam')
+        
+        height = get_height([path], '16', 48000000, 48000400, 0)
+        self.assertEqual(height, 100)
+        
+        # test that we can allow for axis spacing
+        height = get_height([path], '16', 48000000, 48000400, 50)
+        self.assertEqual(height, 150)
+    
+    def test_fileformat_png(self):
+        ''' test we can pick png output format
+        '''
+        ext, surf = fileformat('a.png', 50, 100)
+        self.assertEqual(ext, 'png')
+        self.assertEqual(type(surf), cairo.ImageSurface)
+        
+        # check that we've set the height and width correctly
+        self.assertEqual(surf.get_height(), 100)
+        self.assertEqual(surf.get_width(), 50)
+        
+    def test_fileformat_pdf(self):
+        ''' test we can pick pdf output format
+        '''
+        ext, surf = fileformat('a.pdf', 50, 100)
+        self.assertEqual(ext, 'pdf')
+        self.assertEqual(type(surf), cairo.PDFSurface)
+        
+    def test_fileformat_svg(self):
+        ''' test we can pick svg output format
+        '''
+        ext, surf = fileformat('a.svg', 50, 100)
+        self.assertEqual(ext, 'svg')
+        self.assertEqual(type(surf), cairo.SVGSurface)
+        
+    def test_fileformat_ps(self):
+        ''' test we can pick ps output format
+        '''
+        ext, surf = fileformat('a.ps', 50, 100)
+        self.assertEqual(ext, 'ps')
+        self.assertEqual(type(surf), cairo.PSSurface)
+        
+    def test_fileformat_unknown(self):
+        ''' test we raise error for unknown output format
+        '''
+        with self.assertRaises(AssertionError):
+            fileformat('a.zzz', 50, 100)
